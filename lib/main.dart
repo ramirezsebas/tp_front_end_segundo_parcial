74b634faecb8ca67ce7cd538823f6b4a34c1a11f
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-10-24 22:41:19 -0300

    Agregamos las rutas al MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tp_front_end_segundo_parcial/screens/ficha_clinica_screen.dart';
 import 'package:tp_front_end_segundo_parcial/screens/login_screen.dart';
+import 'package:tp_front_end_segundo_parcial/screens/pacientes_screen.dart';
+import 'package:tp_front_end_segundo_parcial/screens/reserva_turnos_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,11 +14,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Centro de Rehabilitacion Fisioterapeutica',
       routes: {
-        '/': (context) => const LoginScreen(),
-        '/home': (context) => const FichaClinicaScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/fichas_clinicas': (context) => const FichaClinicaScreen(),
+        '/reserva_turnos': (context) => const ReservaTurnoScreen(),
+        '/pacientes': (context) => const PacientesScreen(),
       },
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/login',
     );
   }
 }
