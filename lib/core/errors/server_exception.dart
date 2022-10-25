cc8e10fa9cf1098b790f8821f3c6d8927f3f10d6
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-10-24 22:08:16 -0300

    Agregamos una excepcion del error del servidor

---
+++ lib/core/errors/server_exception.dart
@@ -0,0 +1,10 @@
+class ServerException implements Exception {
+  final String message;
+
+  ServerException({required this.message});
+
+  @override
+  String toString() {
+    return message;
+  }
+}
