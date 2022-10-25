b5e50052a5f1559cbb4d8df963ab7787feaa29e9
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-10-25 08:36:34 -0300

    Agregamos a la pantalla de la ficha clinica un listado de fichas clinicas obtenidos del API

--- lib/screens/ficha_clinica_screen.dart
+++ lib/screens/ficha_clinica_screen.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:tp_front_end_segundo_parcial/services/ficha_clinica_service.dart';
 
-class FichaClinicaScreen extends StatelessWidget {
+import '../models/ficha_clinica_model.dart';
+
+class FichaClinicaScreen extends StatefulWidget {
   const FichaClinicaScreen({Key? key}) : super(key: key);
 
   @override
+  State<FichaClinicaScreen> createState() => _FichaClinicaScreenState();
+}
+
+class _FichaClinicaScreenState extends State<FichaClinicaScreen> {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    getFichasClinicas();
+  }
+
+  Future<List<FichaClinicaModel>> getFichasClinicas() async {
+    FichaClinicaService fichaClinicaService = FichaClinicaService();
+
+    final fichas = await fichaClinicaService.getFichasClinicas();
+
+    print(fichas);
+
+    return fichas;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     body: Center(
-       child: Text('Ficha Clinica'),
-     ),
+    return Scaffold(
+      body: FutureBuilder<List<FichaClinicaModel>>(
+        future: getFichasClinicas(),
+        initialData: const [],
+        builder: (BuildContext context,
+            AsyncSnapshot<List<FichaClinicaModel>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              final fichas = snapshot.data!;
+
+              return ListView.builder(
+                itemCount: fichas.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final currentFicha = fichas[index];
+                  return ListTile(
+                    title: const Text("Nombre del Paciente"),
+                    subtitle: Text(currentFicha.idCliente?.nombre ?? ""),
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text('No hay datos'),
+              );
+            }
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
