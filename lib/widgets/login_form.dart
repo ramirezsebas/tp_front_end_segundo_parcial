44760143475589a5e6b94f041c4f96b6101cea64
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-10-24 22:08:50 -0300

    Creamos el formulario del login

---
+++ lib/widgets/login_form.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import '../services/auth_service.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: _emailController,
+            decoration: const InputDecoration(
+              labelText: "Email",
+              hintText: "Ingrese su email",
+              icon: Icon(Icons.email),
+            ),
+          ),
+          TextFormField(
+            controller: _passwordController,
+            decoration: const InputDecoration(
+              labelText: "Contraseña",
+              hintText: "Ingrese su contraseña",
+              icon: Icon(Icons.lock),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              AuthService authService = AuthService();
+              try {
+                final isValidUser = await authService.login(
+                    _emailController.text, _passwordController.text);
+
+                if (isValidUser) {
+                  Navigator.pushReplacementNamed(context, '/home');
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text('Usuario o contraseña incorrectos'),
+                    ),
+                  );
+                }
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Error al iniciar sesión'),
+                  ),
+                );
+              }
+            },
+            child: const Text('Login'),
+          ),
+        ],
+      ),
+    );
+  }
+}
