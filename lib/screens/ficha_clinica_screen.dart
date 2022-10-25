6ca356720d308baec021eef06d037a3e1ec7081f
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-10-25 08:53:49 -0300

    Agregamos un Card en el listado de fichas clinicas

--- lib/screens/ficha_clinica_screen.dart
+++ lib/screens/ficha_clinica_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tp_front_end_segundo_parcial/services/ficha_clinica_service.dart';
+import 'package:tp_front_end_segundo_parcial/widgets/custom_card.dart';
 
 import '../models/ficha_clinica_model.dart';
+import '../widgets/card_footer.dart';
+import '../widgets/card_text.dart';
 
 class FichaClinicaScreen extends StatefulWidget {
   const FichaClinicaScreen({Key? key}) : super(key: key);
@@ -41,11 +44,50 @@ class _FichaClinicaScreenState extends State<FichaClinicaScreen> {
 
               return ListView.builder(
                 itemCount: fichas.length,
+                padding: const EdgeInsets.all(8),
                 itemBuilder: (BuildContext context, int index) {
                   final currentFicha = fichas[index];
-                  return ListTile(
-                    title: const Text("Nombre del Paciente"),
-                    subtitle: Text(currentFicha.idCliente?.nombre ?? ""),
+                  return CustomCard(
+                    cardData: Column(
+                      children: [
+                        CardText(
+                          label: "ID Ficha",
+                          text: currentFicha.idFichaClinica.toString(),
+                        ),
+                        CardText(
+                          label: "Fecha y Hora",
+                          text: currentFicha.fechaHora.toString(),
+                        ),
+                        CardText(
+                          label: "Motivo de Consulta",
+                          text: currentFicha.motivoConsulta.toString(),
+                        ),
+                        CardText(
+                          label: "Diagnostico",
+                          text: currentFicha.diagnostico.toString(),
+                        ),
+                        CardText(
+                          label: "Observacion",
+                          text: currentFicha.observacion.toString(),
+                        ),
+                        CardText(
+                          label: "Local",
+                          text: currentFicha.idLocal?.nombreEmpresa ??
+                              "Nombre del Local identificado",
+                        ),
+                        CardText(
+                          label: "Empleado",
+                          text: currentFicha.idEmpleado?.nombre ??
+                              "Nombre del Empleado identificado",
+                        ),
+                        CardText(
+                          label: "Cliente",
+                          text: currentFicha.idCliente?.nombre ??
+                              "Nombre del Cliente identificado",
+                        ),
+                        const CardFooter(),
+                      ],
+                    ),
                   );
                 },
               );
