d1dccf162610a59da97f2600558bf52d78240f04
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-04 02:22:29 -0300

    Creamos un DTO de ficha clinica

---
+++ lib/models/ficha_clinica_dto.dart
@@ -0,0 +1,42 @@
+class FichaClinicaDto {
+  final String motivoConsulta;
+  final String diagnostico;
+  final String observacion;
+  final String idEmpleado;
+  final String idCliente;
+  final String idTipoProducto;
+
+  FichaClinicaDto({
+    required this.motivoConsulta,
+    required this.diagnostico,
+    required this.observacion,
+    required this.idEmpleado,
+    required this.idCliente,
+    required this.idTipoProducto,
+  });
+
+  factory FichaClinicaDto.fromJson(Map<String, dynamic> json) =>
+      FichaClinicaDto(
+        motivoConsulta: json["motivoConsulta"],
+        diagnostico: json["diagnostico"],
+        observacion: json["observacion"],
+        idEmpleado: json["idEmpleado"],
+        idCliente: json["idCliente"],
+        idTipoProducto: json["idTipoProducto"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "motivoConsulta": motivoConsulta,
+        "diagnostico": diagnostico,
+        "observacion": observacion,
+        "idEmpleado": {
+          "idPersona": idEmpleado,
+        },
+        "idCliente": {
+          "idPersona": idCliente,
+        },
+        "idTipoProducto": {
+          "idTipoProducto": idTipoProducto,
+        },
+      };
+}
