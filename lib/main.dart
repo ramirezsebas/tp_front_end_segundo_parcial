9fc1a083d2864fcbfefa263f2e648d91cb95b9e6
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-04 01:25:55 -0300

    Inicializamos el GetStorage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:tp_front_end_segundo_parcial/screens/ficha_clinica_screen.dart';
 import 'package:tp_front_end_segundo_parcial/screens/home_screen.dart';
 import 'package:tp_front_end_segundo_parcial/screens/login_screen.dart';
 import 'package:tp_front_end_segundo_parcial/screens/pacientes_screen.dart';
 import 'package:tp_front_end_segundo_parcial/screens/reserva_turnos_screen.dart';
+import 'package:tp_front_end_segundo_parcial/screens/splash_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await GetStorage.init();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,13 +21,14 @@ class MyApp extends StatelessWidget {
       title: 'Centro de Rehabilitacion Fisioterapeutica',
       routes: {
         '/': (context) => const HomeScreen(),
+        '/splash': (context) => const SplashScreen(),
         '/login': (context) => const LoginScreen(),
         '/fichas_clinicas': (context) => const FichaClinicaScreen(),
         '/reserva_turnos': (context) => const ReservaTurnoScreen(),
         '/pacientes': (context) => const PacientesScreen(),
       },
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/splash',
     );
   }
 }
