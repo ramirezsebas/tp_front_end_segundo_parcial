b43a428a4a2be6dd5ba5c77fa682db6a3cc27a15
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-11-04 01:27:19 -0300

    Agregamos la funcion de logout Agregamos el write del token del usuario cuando se hace un login

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,15 +1,24 @@
+import 'package:get_storage/get_storage.dart';
 import 'package:tp_front_end_segundo_parcial/services/persona_service.dart';
 
 import '../models/persona_model.dart';
 
 class AuthService {
-  PersonaService personaService = PersonaService();
+  final PersonaService personaService = PersonaService();
+  final box = GetStorage();
 
   Future<bool> login(String usuario, String password) async {
     final personas = await personaService.getPersonasDelSistema();
-    final List<PersonaModel> persona =
-        personas.where((element) => element.nombre == usuario).toList();
+    final PersonaModel persona =
+        personas.where((element) => element.usuarioLogin == usuario).first;
 
-    return persona.isNotEmpty;
+    if (persona.usuarioLogin != null) {
+      box.write('token', persona.usuarioLogin);
+    }
+    return persona.usuarioLogin != null;
+  }
+
+  Future<void> logout() async {
+    box.remove('token');
   }
 }
